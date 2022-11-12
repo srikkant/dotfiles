5e3654f253c14f234dd0684f50c85d77ca1e5fba
Srikkant Srinivasan <srikkant@live.com>
2022-11-12 20:33:29 +0530

    feat: added wezterm

--- alacritty/change-theme.sh
+++
@@ -1,5 +0,0 @@
-#! /bin/sh
-
-echo "Changing theme: $1"
-rm -rf .activetheme.yml
-ln -s ./themes/"$1".yml .activetheme.yml
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,4 +1,5 @@
 local wezterm = require("wezterm")
+local act = wezterm.action
 
 -- There are custom color schemes defined in the color_schemes directory relative to this file.
 local color_scheme_dirs = {
@@ -7,9 +8,23 @@ local color_scheme_dirs = {
 
 local color_scheme = "Everforest Medium Dark"
 
+local keys = {
+	{ key = "_", mods = "SHIFT|ALT", action = act.SplitVertical({ domain = "CurrentPaneDomain" }) },
+	{ key = "|", mods = "SHIFT|ALT", action = act.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+	{ key = "LeftArrow", mods = "ALT", action = act.ActivatePaneDirection("Left") },
+	{ key = "LeftArrow", mods = "SHIFT|ALT", action = act.AdjustPaneSize({ "Left", 1 }) },
+	{ key = "RightArrow", mods = "ALT", action = act.ActivatePaneDirection("Right") },
+	{ key = "RightArrow", mods = "SHIFT|ALT", action = act.AdjustPaneSize({ "Right", 1 }) },
+	{ key = "UpArrow", mods = "ALT", action = act.ActivatePaneDirection("Up") },
+	{ key = "UpArrow", mods = "SHIFT|ALT", action = act.AdjustPaneSize({ "Up", 1 }) },
+	{ key = "DownArrow", mods = "ALT", action = act.ActivatePaneDirection("Down") },
+	{ key = "DownArrow", mods = "SHIFT|ALT", action = act.AdjustPaneSize({ "Down", 1 }) },
+}
+
 return {
 	color_scheme_dirs = color_scheme_dirs,
 	color_scheme = color_scheme,
 	enable_tab_bar = false,
 	font_size = 15.0,
+	keys = keys,
 }
