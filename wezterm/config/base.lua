7a81ef387104c1ac5c76038d1103fb07909ac270
Srikkant Srinivasan <srikkant@live.com>
2022-11-23 16:57:09 +0530

    feat: added everforest light

--- wezterm/config/base.lua
+++ wezterm/config/base.lua
@@ -2,11 +2,19 @@ local wezterm = require("wezterm")
 local keys = require("config.keys").keys
 local ssh_domains = require("config.ssh").ssh_domains
 
+function scheme_for_appearance(appearance)
+	if appearance:find("Dark") then
+		return "Everforest Medium Dark"
+	else
+		return "Everforest Medium Dark"
+	end
+end
+
 return {
 	color_scheme_dirs = {
 		wezterm.home_dir .. "/.config/wezterm/color_schemes",
 	},
-	color_scheme = "Everforest Medium Dark",
+	color_scheme = scheme_for_appearance(wezterm.gui.get_appearance()),
 	enable_tab_bar = true,
 	font = wezterm.font_with_fallback({ "Iosevka Extended" }),
 	keys = keys,
