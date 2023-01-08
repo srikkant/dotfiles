5948134c6a4ac62193b3ee0c504125a9632a0572
Srikkant Srinivasan <srikkant@live.com>
2023-01-09 00:16:10 +0530

    chore: updated font

--- wezterm/config/platform.lua
+++ wezterm/config/platform.lua
@@ -29,15 +29,15 @@ local config = {
 		inactive_titlebar_bg = bg,
 		active_titlebar_bg = active_bg,
 	},
+	font = wezterm.font_with_fallback({
+		{ family = "Hasklig" },
+	}),
 }
 
 if wezterm.target_triple == "x86_64-pc-windows-msvc" then
 	config.default_prog = { "nu" }
 	config.font_size = 10
 	config.line_height = 1.1
-	--  config.font = wezterm.font_with_fallback({
-	--  { family = "Iosevka Extended", weight = "Medium" },
-	--  })
 else
 	config.font_size = 14
 	config.line_height = 1.1
