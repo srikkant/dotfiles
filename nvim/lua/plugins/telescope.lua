dbb855387175271ba3fb376432f62d01daca4206
Srikkant Srinivasan <srikkant@live.com>
2023-02-19 22:20:18 +0530

    feat: added filebrowser telescope

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -2,11 +2,42 @@ return { -- add telescope-fzf-native
     {
         "telescope.nvim",
         dependencies = {
-            "nvim-telescope/telescope-fzf-native.nvim",
-            build = "make",
-            config = function()
-                require("telescope").load_extension("fzf")
-            end,
+            {
+                "nvim-telescope/telescope-fzf-native.nvim",
+                build = "make",
+                config = function()
+                    require("telescope").load_extension("fzf")
+                end,
+            },
+            {
+                "nvim-telescope/telescope-file-browser.nvim",
+                config = function()
+                    require("telescope").load_extension("file_browser")
+                end,
+            },
+        },
+        keys = {
+            {
+                "<leader><leader>",
+                function()
+                    require("telescope.builtin").resume()
+                end,
+            },
+            {
+                "<leader>fb",
+                function()
+                    require("telescope").extensions.file_browser.file_browser({
+                        path = "%:p:h",
+                        cwd = vim.fn.expand("%:p:h"),
+                        respect_gitignore = false,
+                        hidden = true,
+                        grouped = true,
+                        theme = "ivy",
+                        initial_mode = "normal",
+                    })
+                end,
+                desc = "Open file browser",
+            },
         },
     },
 }
