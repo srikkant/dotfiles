e0dc0fe0e0a4869d1a078ce50555eebf44ee09a6
Srikkant Srinivasan <srsrinivasan@informatica.com>
2023-03-29 14:26:45 +0530

    feat: added neovide

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,3 +1,13 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+    pattern = { "*.ts", "*.tsx" },
+    callback = function()
+        local typescript = require("typescript")
+        typescript.actions.organizeImports({ sync = true })
+        typescript.actions.addMissingImports({ sync = true })
+        typescript.actions.fixAll({ sync = true })
+    end,
+})
---
+++ nvim/lua/config/neovide.lua
@@ -0,0 +1,5 @@
+if vim.g.neovide then
+    vim.o.guifont = "JetBrains Mono:h11"
+    vim.opt.linespace = 4
+    vim.g.neovide_cursor_trail_size = 0
+end
