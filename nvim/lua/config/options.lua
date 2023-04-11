97bb1cf90f42862945e78776e9a057ce4b36a42b
Srinivasan, Srikkant <srsrinivasan@informatica.com>
2023-04-11 19:54:05 +0530

    feat: moved to lspsaga

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -4,5 +4,3 @@ vim.opt.background = "dark"
 
 vim.opt.shiftwidth = 4 -- Size of an indent
 vim.opt.tabstop = 4 -- Number of spaces tabs count for
-
-vim.g.moonflyTransparent = true
--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -2,9 +2,26 @@ return {
     {
         {
             "neovim/nvim-lspconfig",
+            init = function()
+                local keys = require("lazyvim.plugins.lsp.keymaps").get()
+                -- disable keymaps
+                keys[#keys + 1] = { "gr", false }
+                keys[#keys + 1] = { "gd", false }
+                keys[#keys + 1] = { "gp", false }
+                keys[#keys + 1] = { "gt", false }
+                keys[#keys + 1] = { "cd", false }
+                keys[#keys + 1] = { "]e", false }
+                keys[#keys + 1] = { "[e", false }
+                keys[#keys + 1] = { "]E", false }
+                keys[#keys + 1] = { "[E", false }
+                -- disabled keymaps
+            end,
             ---@class PluginLspOpts
             opts = {
                 ---@type lspconfig.options
+                opts = {
+                    format = { timeout_ms = 1000 },
+                },
                 servers = {
                     svelte = {},
                 },
@@ -57,6 +74,47 @@ return {
                     },
                 }
             end,
+        },
+        {
+            "glepnir/lspsaga.nvim",
+            event = "LspAttach",
+            config = function()
+                require("lspsaga").setup({})
+            end,
+            dependencies = { { "nvim-tree/nvim-web-devicons" } },
+            keys = {
+                { "<leader>ca", "<cmd>Lspsaga code_action<CR>", desc = "Code Action", mode = { "n", "v" } },
+                { "gh", "<cmd>Lspsaga lsp_finder<CR>", desc = "Find References" },
+                { "gr", "<cmd>Lspsaga rename<CR>", desc = "Rename" },
+                { "gr", "<cmd>Lspsaga rename ++project<CR>", desc = "Rename" },
+                { "gp", "<cmd>Lspsaga peek_definition<CR>", desc = "Peek Definition" },
+                { "gd", "<cmd>Lspsaga goto_definition<CR>", desc = "Goto Definition" },
+                { "gt", "<cmd>Lspsaga peek_type_definition<CR>", desc = "Peek Type definition" },
+                { "gt", "<cmd>Lspsaga goto_type_definition<CR>", desc = "Goto Type definition" },
+                { "<leader>cd", "<cmd>Lspsaga show_line_diagnostics<CR>", desc = "Line Diagnostics" },
+                { "<leader>cb", "<cmd>Lspsaga show_buf_diagnostics<CR>", desc = "Buffer Diagnostics" },
+                { "<leader>cw", "<cmd>Lspsaga show_workspace_diagnostics<CR>", desc = "Workspace Diagnostics" },
+                { "<leader>cc", "<cmd>Lspsaga show_cursor_diagnostics<CR>", desc = "Cursor Diagnostics" },
+                { "[e", "<cmd>Lspsaga diagnostic_jump_prev<CR>", desc = "Prev Diagnostic" },
+                { "]e", "<cmd>Lspsaga diagnostic_jump_next<CR>", desc = "Next Diagnostic" },
+                {
+                    "[E",
+                    function()
+                        require("lspsaga.diagnostic"):goto_prev({ severity = vim.diagnostic.severity.ERROR })
+                    end,
+                    desc = "Previous Error",
+                },
+                {
+                    "]E",
+                    function()
+                        require("lspsaga.diagnostic"):goto_next({ severity = vim.diagnostic.severity.ERROR })
+                    end,
+                    desc = "Next Error",
+                },
+                { "<leader>o", "<cmd>Lspsaga outline<CR>", desc = "Toggle Outline" },
+                { "<leader>ci", "<cmd>Lspsaga incoming_calls<CR>", desc = "Incoming calls" },
+                { "<leader>co", "<cmd>Lspsaga outgoing_calls<CR>", desc = "Outgoing calls" },
+            },
         },
     },
 }
