a1e159f444fd20946d4bc059a4114c35670dd719
Srikkant Srinivasan <srikkant@live.com>
2023-05-08 16:57:59 +0530

    feat: added onehalf

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,10 +1,18 @@
 return {
-    { "catppuccin/nvim", name = "catppuccin", lazy = true, priority = 1000 },
-
+    {
+        "navarasu/onedark.nvim",
+        lazy = true,
+        priority = 1000,
+        config = function()
+            require("onedark").load({
+                style = "darker",
+            })
+        end,
+    },
     {
         "LazyVim/LazyVim",
         opts = {
-            colorscheme = "catppuccin-mocha",
+            colorscheme = "onedark",
         },
     },
 }
