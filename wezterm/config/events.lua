8f585dd6c8a3311beeb438a060462120b49af4f6
Srikkant Srinivasan <srikkant@live.com>
2023-07-06 15:28:01 +0530

    chore: simplified wezterm config

--- wezterm/config/events.lua
+++
@@ -1,6 +0,0 @@
-local wezterm = require("wezterm")
-
-wezterm.on("gui-startup", function()
-	local _, _, window = wezterm.mux.spawn_window({})
-	window:gui_window():maximize()
-end)
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,5 +1,42 @@
-local stdlib = require("lib.std").stdlib
+local wezterm = require 'wezterm'
+local config = {}
 
-require("config.events")
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
 
-return stdlib.merge(require("config.base"), require("config.platform"))
+config.color_scheme = 'Builtin Pastel Dark'
+config.default_cursor_style = 'BlinkingUnderline'
+config.font = wezterm.font 'Cascadia Code PL'
+config.font_size = 10.5
+config.line_height = 1.4
+config.cell_width = 1.1
+config.tab_bar_at_bottom = true
+config.window_decorations = 'RESIZE' 
+
+config.window_frame = {
+  active_titlebar_bg = '#222222',
+  font_size = 9.5,
+}
+
+config.window_padding = {
+  left = 16,
+  right = 16,
+  top = 16,
+  bottom = 16,
+}
+
+config.background = {
+  {
+    source = { Color = "#111111" },
+    opacity = 1,
+    height = "100%",
+    width = "100%"
+  },
+  {
+    source = { File = './wallpaper.jpg' },
+    opacity = 0.075,
+  },
+}
+
+return config
