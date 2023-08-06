d96a9755713e17247fa737571b46b8f1ff7c0cb2
Srikkant Srinivasan <srikkant@live.com>
2023-08-06 18:03:25 +0530

    fix: updated tmux theme

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -6,17 +6,18 @@ local font_size = os.getenv("TERMINAL_FONT_SIZE") or 13;
 
 config.default_cursor_style = "BlinkingUnderline"
 config.color_scheme = "Gruvbox Light"
-config.font = wezterm.font "Cascadia Code PL"
+config.font = wezterm.font ("Cascadia Code PL", { weight  = "DemiBold" })
 config.font_size = font_size
 config.line_height = 1.6
-config.cell_width = 1.1
+config.cell_width = 1.05
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
-config.window_padding = { left = 24, right = 16, top = 0, bottom = 0 }
+config.window_padding = { left = 16, right = 16, top = 0, bottom = 0 }
 config.set_environment_variables = { APPEARANCE = "light" }
 
 -- Dark mode overrides. 
 if appearance:find "Dark" then
+  config.font = wezterm.font ("Cascadia Code PL", { weight  = "Regular" })
   config.set_environment_variables["APPEARANCE"] = "dark"
   config.color_scheme = "Gruvbox Dark"
 end
