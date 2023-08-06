77c0805ccc5de08b01e9e93098db8a881b630187
Srikkant Srinivasan <srikkant@live.com>
2023-08-06 11:12:47 +0530

    fix: added gruvbox dynamic

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -5,21 +5,20 @@ local appearance = wezterm.gui.get_appearance()
 local font_size = os.getenv("TERMINAL_FONT_SIZE") or 13;
 
 config.default_cursor_style = "BlinkingUnderline"
-config.color_scheme = "Catppuccin Latte"
+config.color_scheme = "Gruvbox Light"
 config.font = wezterm.font "Cascadia Code PL"
 config.font_size = font_size
 config.line_height = 1.6
 config.cell_width = 1.1
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
-config.window_padding = { left = 16, right = 16, top = 0, bottom = 0 }
-config.set_environment_variables = { APPEARANCE = "light", CATPPUCCIN_FLAVOUR = "latte" }
+config.window_padding = { left = 24, right = 16, top = 0, bottom = 0 }
+config.set_environment_variables = { APPEARANCE = "light" }
 
 -- Dark mode overrides. 
 if appearance:find "Dark" then
   config.set_environment_variables["APPEARANCE"] = "dark"
-  config.set_environment_variables["CATPPUCCIN_FLAVOUR"] = "mocha"
-  config.color_scheme = "Catppuccin Mocha"
+  config.color_scheme = "Gruvbox Dark"
 end
 
 return config
