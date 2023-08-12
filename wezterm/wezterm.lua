3772dd729440774fc07ea5f577e18b874bae575d
Srikkant Srinivasan <srikkant@live.com>
2023-08-12 12:30:01 +0530

    feat: added envgen for wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,25 +1,24 @@
 local wezterm = require "wezterm"
-local config = wezterm.config_builder()
+local env = require "env"
 
-local appearance = wezterm.gui.get_appearance()
-local font_size = os.getenv("TERMINAL_FONT_SIZE") or "13";
+local config = wezterm.config_builder()
 
 config.default_cursor_style = "BlinkingUnderline"
 config.color_scheme = "Gruvbox Light"
-config.font = wezterm.font ("Cascadia Code PL", { weight  = "DemiBold" })
-config.font_size = tonumber(font_size)
-config.line_height = 1.6
-config.cell_width = 1.05
+config.font = wezterm.font("Cascadia Code PL")
+config.font_size = env["font_size"]
+config.line_height = env["line_height"]
+config.cell_width = env["cell_width"]
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
 config.window_padding = { left = 16, right = 16, top = 0, bottom = 0 }
 config.set_environment_variables = { APPEARANCE = "light" }
 
--- Dark mode overrides. 
-if appearance:find "Dark" then
-  config.font = wezterm.font ("Cascadia Code PL", { weight  = "Regular" })
+-- Dark mode overrides.
+if env["appearance"]:find "Dark" then
+  config.font = wezterm.font("Cascadia Code PL")
   config.set_environment_variables["APPEARANCE"] = "dark"
-  config.color_scheme = "Gruvbox dark, hard (base16)"
+  config.color_scheme = "Gruvbox Dark (Gogh)"
 end
 
 return config
