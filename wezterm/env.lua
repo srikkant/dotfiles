4c1d55aeacc1603b194e1154ceb91338130d82da
Srikkant Srinivasan <srikkant@live.com>
2023-08-14 08:54:26 +0530

    feat(exp): added polybar and i3

--- wezterm/env.lua
+++ wezterm/env.lua
@@ -10,11 +10,10 @@ local function init_key(envKey, default_value)
 	return value
 end
 
-
 -- add the required environment initialization here with default values.
 module["appearance"] = init_key("TERMINAL_APPEARANCE", wezterm.gui.get_appearance())
-module["font_size"] = tonumber(init_key("TERMINAL_FONT_SIZE", 13))
-module["cell_width"] = tonumber(init_key("TERMINAL_CELL_SPACING", 1.1))
-module["line_height"] = tonumber(init_key("TERMINAL_LINE_HEIGHT", 1.6))
+module["font_size"] = tonumber(init_key("TERMINAL_FONT_SIZE", 12))
+module["cell_width"] = tonumber(init_key("TERMINAL_CELL_SPACING", 1))
+module["line_height"] = tonumber(init_key("TERMINAL_LINE_HEIGHT", 1.4))
 
 return module
