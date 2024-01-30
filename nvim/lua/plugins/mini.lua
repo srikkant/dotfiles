14db1a50a74f16bf5670cc6fa46cb6d42763bf09
Srikkant Srinivasan <srikkant@live.com>
2024-01-31 00:44:45 +0530

    chore: added dot files hide to mini.files

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -42,12 +42,6 @@ return {
                 },
             })
 
-            require("mini.files").setup({
-                windows = {
-                    preview = true,
-                },
-            })
-
             require("mini.basics").setup({
                 {
                     options = {
@@ -65,6 +59,36 @@ return {
                     },
                 },
             })
+
+            local show_dotfiles = true
+            local filter_show = function(_)
+                return true
+            end
+            local filter_hide = function(fs_entry)
+                return not vim.startswith(fs_entry.name, ".")
+            end
+
+            local toggle_dotfiles = function()
+                show_dotfiles = not show_dotfiles
+                local new_filter = show_dotfiles and filter_show or filter_hide
+                require("mini.files").refresh({ content = { filter = new_filter } })
+            end
+
+            vim.api.nvim_create_autocmd("User", {
+                pattern = "MiniFilesBufferCreate",
+                callback = function(args)
+                    local buf_id = args.data.buf_id
+                    -- Tweak left-hand side of mapping to your liking
+                    vim.keymap.set("n", "g.", toggle_dotfiles, { buffer = buf_id })
+                end,
+            })
+
+            require("mini.files").setup({
+                windows = {
+                    preview = true,
+                },
+            })
+
         end,
     },
 }
