eb278e8935ed0e200a4ba04ae0e6448bafdbdac1
Srikkant Srinivasan <srikkant@live.com>
2024-01-30 16:09:31 +0530

    feat: redoing nvim config

---
+++ nvim/lua/opts.lua
@@ -0,0 +1,15 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+vim.o.hlsearch = false
+vim.o.clipboard = 'unnamedplus'
+vim.o.breakindent = true
+vim.o.undofile = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
+vim.o.completeopt = 'menuone,noselect'
+vim.o.termguicolors = true
+
+vim.wo.number = true
+vim.wo.signcolumn = 'yes'
+vim.wo.relativenumber = true
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,53 +1,43 @@
-return { -- add telescope-fzf-native
+return {
+  'nvim-telescope/telescope.nvim',
+  branch = '0.1.x',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
     {
-        "telescope.nvim",
-        dependencies = {
-            {
-                "nvim-telescope/telescope-fzf-native.nvim",
-                build = "make",
-                config = function()
-                    require("telescope").load_extension("fzf")
-                end,
-            },
-            {
-                "nvim-telescope/telescope-file-browser.nvim",
-                config = function()
-                    require("telescope").load_extension("file_browser")
-                end,
-            },
-        },
-        init = function()
-            require("telescope").load_extension("flutter")
-        end,
-        keys = {
-            {
-                "<leader><leader>",
-                function()
-                    require("telescope.builtin").resume()
-                end,
-            },
-            {
-                "<leader>fd",
-                function()
-                    require("telescope").extensions.flutter.commands()
-                end,
-                desc = "Flutter Tools (Flutter projects)",
-            },
-            {
-                "<leader>fb",
-                function()
-                    require("telescope").extensions.file_browser.file_browser({
-                        path = "%:p:h",
-                        cwd = vim.fn.expand("%:p:h"),
-                        respect_gitignore = false,
-                        hidden = true,
-                        grouped = true,
-                        theme = "ivy",
-                        initial_mode = "normal",
-                    })
-                end,
-                desc = "Open file browser",
-            },
-        },
+      'nvim-telescope/telescope-fzf-native.nvim',
+      build = 'make',
+      config = function()
+        require("telescope").load_extension("fzf")
+      end,
     },
+  },
+  keys = {
+      { 
+        "<leader>b", 
+        function() 
+          require("telescope.builtin").buffers() 
+        end, 
+        { 
+          desc = 'List [b]uffers' 
+        }
+      },
+      { 
+        "<leader>f", 
+        function() 
+          require("telescope.builtin").find_files() 
+        end, 
+        { 
+          desc = 'List [f]iles' 
+        }
+      },
+      { 
+        "<leader>'", 
+        function() 
+          require("telescope.builtin").resume() 
+        end, 
+        { 
+          desc = 'Reopen last picker' 
+        }
+      }
+  }
 }
