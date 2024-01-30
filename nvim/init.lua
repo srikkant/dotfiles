203adc8c36d27c18215019d8f65bcea3fefc37f6
Srikkant Srinivasan <srikkant@live.com>
2024-01-30 16:09:31 +0530

    feat: redoing nvim config

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,6 @@
-require("config.lazy")
-require("config.neovide")
+require("opts");
+require("lazyvim");
+require("keymaps");
+
+-- set the color scheme at end!
+vim.cmd[[colorscheme tokyonight-night]]
