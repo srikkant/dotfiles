e61ed874f57e29e3c138e90a3eb1634ee8e0c1be
Srikkant Srinivasan <srikkant@live.com>
2024-11-12 16:28:22 +0530

    fix: alacritty update

--- .config/nvim/lua/srikkant/plugins.lua
+++ .config/nvim/lua/srikkant/plugins.lua
@@ -18,7 +18,6 @@ local deps = require("mini.deps")
 deps.setup()
 
 -- indirect dependencies
-deps.add("nvim-tree/nvim-web-devicons")
 deps.add("kevinhwang91/promise-async")
 deps.add("nvim-lua/plenary.nvim")
 deps.add("nvim-neotest/nvim-nio")
@@ -26,7 +25,6 @@ deps.add("nvim-neotest/nvim-nio")
 -- dev dependencies
 deps.add("folke/neodev.nvim")
 
-deps.add("lewis6991/gitsigns.nvim")
 deps.add("kevinhwang91/nvim-ufo")
 deps.add("neovim/nvim-lspconfig")
 deps.add("L3MON4D3/LuaSnip")
@@ -38,6 +36,7 @@ deps.add("mfussenegger/nvim-lint")
 deps.add("hrsh7th/nvim-cmp")
 deps.add("hrsh7th/cmp-nvim-lsp")
 deps.add("hrsh7th/cmp-path")
+deps.add("github/copilot.vim")
 deps.add("stevearc/conform.nvim")
 deps.add("nvim-treesitter/nvim-treesitter")
 deps.add("rose-pine/neovim")
@@ -46,7 +45,6 @@ deps.add("mfussenegger/nvim-dap")
 deps.add("leoluz/nvim-dap-go")
 
 local colorscheme = require("rose-pine")
-local gitsigns = require("gitsigns")
 local ufo = require("ufo")
 local lsp_zero = require("lsp-zero")
 local trouble = require("trouble")
@@ -65,9 +63,11 @@ local pairs = require("mini.pairs")
 local pick = require("mini.pick")
 local statusline = require("mini.statusline")
 local surround = require("mini.surround")
+local git = require("mini.git")
+local diff = require("mini.diff")
+local icons = require("mini.icons")
 local neodev = require("neodev")
 local treesitter_configs = require("nvim-treesitter.configs")
-local web_devicons = require("nvim-web-devicons")
 local dap_go = require("dap-go")
 local debugprint = require("debugprint")
 
@@ -92,8 +92,6 @@ neodev.setup()
 -- Some essential pure UI plugins
 --
 ufo.setup()
-gitsigns.setup()
-web_devicons.setup()
 
 dap_go.setup()
 debugprint.setup()
@@ -103,14 +101,17 @@ debugprint.setup()
 --
 basics.setup({})
 comment.setup({})
+icons.setup({})
 extra.setup({})
 pairs.setup({})
 surround.setup({})
 pick.setup({})
 statusline.setup({})
 bracketed.setup({})
-indentscope.setup({ draw = { animation = indentscope.gen_animation.none() } })
-files.setup({ windows = { max_number = 3, width_focus = 50, width_nofocus = 40, width_preview = 80, preview = true } })
+diff.setup({})
+git.setup({})
+indentscope.setup({})
+files.setup({})
 
 --
 -- Treesitter configurations
@@ -147,6 +148,7 @@ cmp.setup({
         { name = "nvim_lsp" },
         { name = "path" },
         { name = "buffer" },
+        { name = "copilot" },
     },
     window = {
         completion = cmp.config.window.bordered(),
