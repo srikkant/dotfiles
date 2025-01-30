597cdd448102fe483603df96f097849cd1d1e257
Srikkant Srinivasan <srikkant@live.com>
2025-01-30 23:26:18 +0530

    chore: added formatters

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -54,10 +54,13 @@ require("blink.cmp").setup()
 local null_ls = require("null-ls")
 null_ls.setup({
     sources = {
+        null_ls.builtins.formatting.clang_format,
         null_ls.builtins.formatting.goimports,
         null_ls.builtins.formatting.prettierd,
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.sql_formatter,
+        null_ls.builtins.formatting.buildifier,
+        null_ls.builtins.diagnostics.buildifier,
     },
 })
 
