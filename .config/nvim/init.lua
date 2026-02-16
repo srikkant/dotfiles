bb0748590a86e61df7b3ab336f43dd777db7c790
Srikkant Srinivasan <srikkant@live.com>
2026-02-16 16:33:25 +0530

    feat: yazi.nvim

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -15,35 +15,42 @@ vim.opt.breakindent = true
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.completeopt = "menuone,noselect,popup"
+vim.opt.wrap = false
 vim.opt.wildmode = "longest:full,full"
 vim.opt.wildoptions:append("fuzzy")
 vim.opt.wildignore:append({ "*/.git/*", "*/build/*" })
 vim.opt.path:append("**")
+
+vim.g.loaded_netrwPlugin = 1
 
 vim.pack.add({
     "https://github.com/folke/lazydev.nvim",
     "https://github.com/neovim/nvim-lspconfig",
     "https://github.com/supermaven-inc/supermaven-nvim",
     "https://github.com/ibhagwan/fzf-lua",
+    "https://github.com/nvim-lua/plenary.nvim",
+    "https://github.com/mikavilpas/yazi.nvim"
 })
 
 require("lazydev").setup()
 require("fzf-lua").setup()
 require("supermaven-nvim").setup({})
+require("yazi").setup({})
 
-for _, server in ipairs({ "lua_ls", "clangd", "gopls", "gdscript" }) do
+for _, server in ipairs({ "lua_ls", "clangd", "gopls", "ols" }) do
     vim.lsp.enable(server)
 end
 
 vim.keymap.set({ "n", "v" }, "+y", [["+y]])
 vim.keymap.set({ "n", "v" }, "+d", [["_d]])
-vim.keymap.set("n", "-", "<cmd>Explore<cr>")
+vim.keymap.set("n", "-", require("yazi").yazi)
 vim.keymap.set("n", "<leader>cq", vim.diagnostic.setqflist)
 vim.keymap.set("n", "<leader>cl", vim.diagnostic.setloclist)
 vim.keymap.set("n", "<leader>cm", ":make ")
 vim.keymap.set("n", "<leader>cc", "<cmd>make<cr>")
 vim.keymap.set("n", "<leader>ff", "<cmd>FzfLua files<cr>")
 vim.keymap.set("n", "<leader>fb", "<cmd>FzfLua buffers<cr>")
+vim.keymap.set("n", "<leader>/", "<cmd>FzfLua live_grep<cr>")
 vim.keymap.set("n", "<leader><leader>", "<cmd>FzfLua global<cr>")
 
 vim.api.nvim_create_autocmd("LspAttach", {
