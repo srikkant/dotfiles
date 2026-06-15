abebd7e758bc9c9a063e60afda6b05fcee73fa97
Srikkant Srinivasan <srikkant@live.com>
2026-06-15 18:22:45 +0530

    chore: cleanup dms config

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -1,16 +1,14 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 (add-to-list 'default-frame-alist '(alpha-background . 90))
-(add-to-list 'custom-theme-load-path "~/.config/emacs/themes/") ;; DMS puts it's theme here
 
-(setq doom-theme `dank-emacs)
-(setq doom-font (font-spec :family "Geist Mono" :size 13))
-(setq doom-variable-pitch-font (font-spec :family "Geist" :size 13))
+(setq doom-font (font-spec :family "Hasklig" :size 13 :weight 'SemiBold))
 (setq display-line-numbers-type 'relative)
 (setq shell-file-name (executable-find "bash"))
+(setq doom-theme 'doom-gruvbox)
 
 (use-package! ghostel
   :config
-  (map! :leader (:prefix-map "o" "t" #'ghostel)))
+  (map! :leader (:prefix-map ("o" . "open") "t" #'ghostel)))
 
 (use-package! copilot
   :hook ((prog-mode . copilot-mode)
