5d16b86cadbe3d30b430c7e294cb47bc3df16b8f
Srikkant Srinivasan <srikkant@live.com>
2026-07-05 12:18:18 +0530

    chore: config cleanup

--- .config/emacs/early-init.el
+++ .config/emacs/early-init.el
@@ -1,3 +1,25 @@
 ;; -*- lexical-binding: t; -*-
 
 (setq package-enable-at-startup nil)
+(setq gc-cons-threshold 100000000)
+(setq straight-use-package-by-default t)
+
+(setq inhibit-startup-message t)
+(setq frame-resize-pixelwise t)
+(setq ring-bell-function 'ignore)
+(setq display-line-numbers-type 'relative)
+(setq-default tab-width 4)
+
+(setq backup-directory-alist `(("." . ,(concat user-emacs-directory "backups/"))))
+(setq auto-save-file-name-transforms `((".*" ,(concat user-emacs-directory "auto-save/") t)))
+(setq lock-file-name-transforms `((".*" ,(concat user-emacs-directory "lock-files/") t)))
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+
+(setq default-frame-alist
+      '((menu-bar-lines . 0)
+        (tool-bar-lines . 0)
+        (vertical-scroll-bars . nil)
+        (alpha-background . 50)
+        (ns-background-blur . 30)
+        (ns-alpha-elements ns-alpha-all)
+        (undecorated-round . t)))
